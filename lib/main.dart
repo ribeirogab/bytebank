17edcbd99f16ccb8889003d25bf22ab7c7efb6d4
ribeirogab <gblosr@gmail.com>
2020-07-19 20:20:22 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Column(
+      children: <Widget>[
+        Text(
+          'Deliver features faster',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+      ],
+    ),
+  );
+}
